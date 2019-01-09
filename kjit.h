0e301ba68b8fa19829d6d733e90e1ac7b4a6a7e8
fluorinedog <fluorinedog@gmail.com>
2019-01-10 03:17:53 +0800

    format

--- kjit.h
+++ kjit.h
@@ -23,75 +23,77 @@ namespace llvm {
 namespace orc {
 
 class KaleidoscopeJIT {
-private:
-	ExecutionSession ES;
-	RTDyldObjectLinkingLayer ObjectLayer;
-	IRCompileLayer CompileLayer;
-	IRTransformLayer OptimizeLayer;
-	
-	DataLayout DL;
-	MangleAndInterner Mangle;
-	ThreadSafeContext Ctx;
+  private:
+    ExecutionSession ES;
+    RTDyldObjectLinkingLayer ObjectLayer;
+    IRCompileLayer CompileLayer;
+    IRTransformLayer OptimizeLayer;
 
-public:
-	KaleidoscopeJIT(JITTargetMachineBuilder JTMB, DataLayout DL)
-			: ObjectLayer(ES,
-			[]() { return llvm::make_unique<SectionMemoryManager>(); }),
-			CompileLayer(ES, ObjectLayer, ConcurrentIRCompiler(std::move(JTMB))),
-			OptimizeLayer(ES, CompileLayer, optimizeModule),
-			DL(std::move(DL)), Mangle(ES, this->DL),
-			Ctx(llvm::make_unique<LLVMContext>()) {
-		ES.getMainJITDylib().setGenerator(
-				cantFail(DynamicLibrarySearchGenerator::GetForCurrentProcess(DL)));
-	}
-	
-	const DataLayout &getDataLayout() const { return DL; }
-	
-	LLVMContext &getContext() { return *Ctx.getContext(); }
-	
-	static Expected<std::unique_ptr<KaleidoscopeJIT>> Create() {
-		auto JTMB = JITTargetMachineBuilder::detectHost();
-		
-		if (!JTMB)
-			return JTMB.takeError();
-		
-		auto DL = JTMB->getDefaultDataLayoutForTarget();
-		if (!DL)
-			return DL.takeError();
-		
-		return llvm::make_unique<KaleidoscopeJIT>(std::move(*JTMB), std::move(*DL));
-	}
-	
-	Error addModule(std::unique_ptr<Module> M) {
-		return OptimizeLayer.add(ES.getMainJITDylib(),
-				ThreadSafeModule(std::move(M), Ctx));
-	}
-	
-	Expected<JITEvaluatedSymbol> lookup(StringRef Name) {
-		return ES.lookup({&ES.getMainJITDylib()}, Mangle(Name.str()));
-	}
+    DataLayout DL;
+    MangleAndInterner Mangle;
+    ThreadSafeContext Ctx;
 
-private:
-	static Expected<ThreadSafeModule>
-	optimizeModule(ThreadSafeModule TSM, const MaterializationResponsibility &R) {
-		// Create a function pass manager.
-		auto FPM = llvm::make_unique<legacy::FunctionPassManager>(TSM.getModule());
-		
-		// Add some optimizations.
-		FPM->add(createInstructionCombiningPass());
-		FPM->add(createReassociatePass());
-		FPM->add(createGVNPass());
-		FPM->add(createCFGSimplificationPass());
-		FPM->doInitialization();
-		
-		// Run the optimizations over all functions in the module being added to
-		// the JIT.
-		for (auto &F : *TSM.getModule()){
-			FPM->run(F);
-		}
-		return TSM;
-	}
+  public:
+    KaleidoscopeJIT(JITTargetMachineBuilder JTMB, DataLayout DL)
+        : ObjectLayer(ES, []() { return llvm::make_unique<SectionMemoryManager>(); }),
+          CompileLayer(ES, ObjectLayer, ConcurrentIRCompiler(std::move(JTMB))),
+          OptimizeLayer(ES, CompileLayer, optimizeModule),
+          DL(std::move(DL)),
+          Mangle(ES, this->DL),
+          Ctx(llvm::make_unique<LLVMContext>()) {
+        ES.getMainJITDylib().setGenerator(
+            cantFail(DynamicLibrarySearchGenerator::GetForCurrentProcess(DL)));
+    }
+
+    const DataLayout &getDataLayout() const {
+        return DL;
+    }
+
+    LLVMContext &getContext() {
+        return *Ctx.getContext();
+    }
+
+    static Expected<std::unique_ptr<KaleidoscopeJIT>> Create() {
+        auto JTMB = JITTargetMachineBuilder::detectHost();
+
+        if(!JTMB) return JTMB.takeError();
+
+        auto DL = JTMB->getDefaultDataLayoutForTarget();
+        if(!DL) return DL.takeError();
+
+        return llvm::make_unique<KaleidoscopeJIT>(std::move(*JTMB), std::move(*DL));
+    }
+
+    Error addModule(std::unique_ptr<Module> M) {
+        return OptimizeLayer.add(ES.getMainJITDylib(),
+                                 ThreadSafeModule(std::move(M), Ctx));
+    }
+
+    Expected<JITEvaluatedSymbol> lookup(StringRef Name) {
+        return ES.lookup({&ES.getMainJITDylib()}, Mangle(Name.str()));
+    }
+
+  private:
+    static Expected<ThreadSafeModule> optimizeModule(
+        ThreadSafeModule TSM, const MaterializationResponsibility &R) {
+        // Create a function pass manager.
+        auto FPM = llvm::make_unique<legacy::FunctionPassManager>(TSM.getModule());
+
+        // Add some optimizations.
+        FPM->add(createInstructionCombiningPass());
+        FPM->add(createReassociatePass());
+        FPM->add(createGVNPass());
+        FPM->add(createCFGSimplificationPass());
+        FPM->doInitialization();
+
+        // Run the optimizations over all functions in the module being added to
+        // the JIT.
+        for(auto &F : *TSM.getModule()) {
+            FPM->run(F);
+        }
+        return TSM;
+    }
 };
 
-} // end namespace orc
-} // end namespace llvm
+}    // end namespace orc
+}    // end namespace llvm
